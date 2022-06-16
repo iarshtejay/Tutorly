4bdc8f81aae99a9817413ca4c8f606e52b0dc691
Parth Shah <parthshahk@gmail.com>
2022-06-16 09:22:57 -0300

    added quiz components

--- frontend/src/app-router.js
+++ frontend/src/app-router.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Content from "./components/Content";
 import AppLayout from "./views/layout/AppLayout";
+import QuizList from "./components/QuizList";
+import Quiz from "./components/Quiz";
 
 const AppRoutes = (props) => {
-  return (
-    <Router basename={process.env.REACT_APP_BASE_HREF}>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Content />} />
-          <Route path="/home" element={<Content />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename={process.env.REACT_APP_BASE_HREF}>
+            <Routes>
+                <Route path="/" element={<AppLayout />}>
+                    <Route index element={<Content />} />
+                    <Route path="/home" element={<Content />} />
+                    <Route path="/course/quiz" element={<QuizList />} />
+                    <Route path="/course/quiz/:id" element={<Quiz />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRoutes;
