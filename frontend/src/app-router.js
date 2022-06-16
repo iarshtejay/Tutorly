625de03a4b358bb9130569280822f554f6c108b9
dhairya <dh263020@sal.ca>
2022-06-16 15:57:34 -0300

    UI pages

--- frontend/src/app-router.js
+++ frontend/src/app-router.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Content from "./components/Content";
+import Review from "./components/Review";
+import Upload from "./components/Upload";
 import AppLayout from "./views/layout/AppLayout";
 
 const AppRoutes = (props) => {
@@ -8,7 +10,8 @@ const AppRoutes = (props) => {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Content />} />
-          <Route path="/home" element={<Content />} />
+          <Route path="/home" element={<Review />} />
+          <Route path="/upload" element={<Upload />} />
         </Route>
       </Routes>
     </Router>
