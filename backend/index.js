c53091840d585232e7b4cb0a3fe16d832bb89e87
Parampal Singh <pr874463@dal.ca>
2022-07-07 02:01:49 -0300

    Committed backend changes for notifications feature

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-
+const { Server } = require("socket.io");
+const {errorHandler} = require('./src/api/notification/middleware/middleware')
+var cors = require('cors')
 const app = express();
 
 app.use(logger("dev"));
@@ -12,7 +14,8 @@ const db = require("./src/db");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors())
+app.use(errorHandler)
 app.use("/api", require("./src/routes"));
 
 app.get("*", function (req, res) {
@@ -35,6 +38,21 @@ process.on("SIGINT", () => {
 
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+  
+  io.on("connection", (socket) => {
+      console.log(`User Connected: ${socket.id}`)
+  
+      socket.on("send_notification", (data) => {
+          socket.broadcast.emit("receive_notification", data);
+      });
+  });
 
 db.on("connected", () => {
     console.log("Database connected");
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.use('/course',require('../api/courses/routes/routes'));
 router.use('/messaging',require('../api/messaging/controllers/messaging'));
+router.use('/notifications', require('../api/notification/routes/notification'))
 
 module.exports = router;
