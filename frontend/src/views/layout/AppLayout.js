4a59480aca6b7236ee2eb831442597aa3d918bb7
shah3 <hr653326@dal.ca>
2022-07-07 17:59:32 -0300

    Fixed messaging UI

--- frontend/src/views/layout/AppLayout.js
+++ frontend/src/views/layout/AppLayout.js
@@ -9,86 +9,67 @@ import Header from "./components/Header";
 import Navigator from "./components/Navigator";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{" "}
+            {new Date().getFullYear()}.
+        </Typography>
+    );
 }
 
 const drawerWidth = 256;
 
 export default function AppLayout() {
-  const location = useLocation();
-  const tabsConfig = {
-    "chat": {
-      tabs: [
-        {
-          label: "Messages",
-          route: "chat/messages",
-        }
-      ],
-    },
-    "discussion": {
-      tabs: [
-        {
-          label: "Courses",
-          route: "discussion/courses",
+    const location = useLocation();
+    const tabsConfig = {
+        chat: {
+            tabs: [
+                {
+                    label: "Messages",
+                    route: "chat/messages",
+                },
+            ],
         },
-        {
-          label: "Forum",
-          route: "discussion/forum",
+        discussion: {
+            tabs: [
+                {
+                    label: "Courses",
+                    route: "discussion/courses",
+                },
+                {
+                    label: "Forum",
+                    route: "discussion/forum",
+                },
+            ],
         },
-      ],
-    }
-  };
+    };
 
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const isSmUp = useMediaQuery(theme.breakpoints.up("sm"));
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const isSmUp = useMediaQuery(theme.breakpoints.up("sm"));
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
-  return (
-    <Box sx={{ display: "flex", minHeight: "100vh" }}>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-      >
-        {isSmUp ? null : (
-          <Navigator
-            PaperProps={{ style: { width: drawerWidth } }}
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-          />
-        )}
+    return (
+        <Box sx={{ display: "flex", height: "100%" }}>
+            <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+                {isSmUp ? null : <Navigator PaperProps={{ style: { width: drawerWidth } }} variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} />}
 
-        <Navigator
-          PaperProps={{ style: { width: drawerWidth } }}
-          sx={{ display: { sm: "block", xs: "none" } }}
-        />
-      </Box>
-      <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
-        <Header
-          onDrawerToggle={handleDrawerToggle}
-          tabs={tabsConfig[location.pathname.split("/")[1]]?.tabs }
-        />
-        <Box
-          component="main"
-          sx={{ flex: 1, py: 2, px: 2, bgcolor: "#eaeff1"}}
-        >
-          <Outlet />
-        </Box>
-        <Box component="footer" sx={{ p: 2, bgcolor: "#eaeff1" }}>
+                <Navigator PaperProps={{ style: { width: drawerWidth } }} sx={{ display: { sm: "block", xs: "none" } }} />
+            </Box>
+            <Box sx={{ flex: 1, display: "flex", flexDirection: "column", height: "100%" }}>
+                <Header onDrawerToggle={handleDrawerToggle} tabs={tabsConfig[location.pathname.split("/")[1]]?.tabs} />
+                <Box sx={{ flex: 1, py: 2, px: 2, bgcolor: "#eaeff1", height: "100%", maxHeight: "100%", overflow: "auto" }}>
+                    <Outlet />
+                </Box>
+                {/* <Box component="footer" sx={{ p: 2, bgcolor: "#eaeff1" }}>
           <Copyright />
+        </Box> */}
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 }
