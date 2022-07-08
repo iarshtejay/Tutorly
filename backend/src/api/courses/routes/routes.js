4ba4e95b8af01b8eef1f5a87ca909ca23cb338ee
Bharatwaaj Shankaranarayanan <bharatwaaj.1996@gmail.com>
2022-07-08 18:14:56 -0300

    Completed Courses Module

--- backend/src/api/courses/routes/routes.js
+++ backend/src/api/courses/routes/routes.js
@@ -13,7 +13,9 @@ router.get("/all", async (req, res) => {
         const courses = await Service.getAllCourses();
         console.log("courses", courses);
         return res.status(200).json({
-            courses,
+            message: "Fetched all courses",
+            success: true,
+            data: courses,
         });
     } catch (err) {
         return res.status(500).json({
@@ -35,14 +37,16 @@ router.get("/all", async (req, res) => {
         if(!course){
             requestParamCourseNotFound(res);
         }
-        const courses = await Service.createCourse(course);
+        const newCourse = await Service.createCourse(course);
         return res.status(200).json({
-            courses
+            message: "Added a new course",
+            success: true,
+            data: newCourse,
         });
     } catch (err) {
         console.log(err);
         return res.status(500).json({
-            message: "Internal server error. Unable to retrieve courses.",
+            message: "Internal server error. Unable to create the course.",
             success: false,
         });
     }
@@ -50,7 +54,7 @@ router.get("/all", async (req, res) => {
 
 /**
  * @author Bharatwaaj Shankaranarayanan
- * @description Create a new course
+ * @description Update an existing course
  * @params req, res
  * @return courses
  */
@@ -64,19 +68,74 @@ router.get("/all", async (req, res) => {
         if(!course){
             requestParamCourseNotFound(res);
         }
-        const courses = await Service.createCourse(course);
+        const updatedCourse = await Service.updateCourse(courseId, course);
         return res.status(200).json({
-            courses
+            message: "Updated the course",
+            success: true,
+            data: await Service.getSpecificCourse(courseId),
         });
     } catch (err) {
         console.log(err);
         return res.status(500).json({
-            message: "Internal server error. Unable to retrieve courses.",
+            message: "Internal server error. Unable to update the course.",
             success: false,
         });
     }
 });
 
+/**
+ * @author Bharatwaaj Shankaranarayanan
+ * @description Get a specific course
+ * @params req, res
+ * @return courses
+ */
+ router.get("/:id", async (req, res) => {
+    try {
+        const courseId = req.params.id;
+        if(!courseId){
+            requestParamCourseIdNotFound(res);
+        }
+        const course = await Service.getSpecificCourse(courseId);
+        return res.status(200).json({
+            message: "Obtained the specific course",
+            success: true,
+            data: course,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: "Internal server error. Unable to retrieve the course.",
+            success: false,
+        });
+    }
+});
+
+/**
+ * @author Bharatwaaj Shankaranarayanan
+ * @description Delete a specific course
+ * @params req, res
+ * @return courses
+ */
+ router.delete("/delete/:id", async (req, res) => {
+    try {
+        const courseId = req.params.id;
+        if(!courseId){
+            requestParamCourseIdNotFound(res);
+        }
+        await Service.deleteCourse(courseId);
+        return res.status(200).json({
+            message: "Deleted the specific course",
+            success: true,
+            data: courseId,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: "Internal server error. Unable to delete the course.",
+            success: false,
+        });
+    }
+});
 
 
 module.exports = router;
