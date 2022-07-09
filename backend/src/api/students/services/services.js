2754d837cb5885c6125a2e103b1a8f2366dbeca3
Arshdeep Singh <arshtejay@gmail.com>
2022-07-09 17:20:49 -0300

    Enroll and un enroll in a course

--- backend/src/api/students/services/services.js
+++ backend/src/api/students/services/services.js
@@ -1,12 +1,13 @@
 const Student = require("../models/student");
+const Course = require("../models/course");
 
 const checkIfCourseExistsAndUpdate = (student, courseId, archived) => {
-    for (let i=0; i < student.courses.length; i++){
+    for (let i = 0; i < student.courses.length; i++) {
         const course = student.courses[i];
-        if(course._id === courseId){
+        if (course._id === courseId) {
             course.archived = archived;
             return student;
-        } else if ((course._id !== courseId) && (i === student.courseslength - 1) && archived){
+        } else if ((course._id !== courseId) && (i === student.courseslength - 1) && archived) {
             student.courses.push({
                 _id: courseId,
                 progress: 0,
@@ -15,40 +16,70 @@ const checkIfCourseExistsAndUpdate = (student, courseId, archived) => {
             return student;
         }
     };
-    
+
+}
+
+const checkIfCourseExistsAndUnEnroll = (student, course) => {
+    student.courses = (student.courses).filter(course_ => course_._id !== course._id)
+    course.students = (course.students).filter(student_ => student_.id !== student.id)
+    return { student: student, course: course }
+}
+
+const checkIfCourseExistsAndEnroll = (student, course) => {
+    if ((student.courses).find(course_ => course_.id === course.id)) {
+        return { student: student, course: course }
+    }
+    (student.courses).push(course)
+        (course.students).push(student)
+
+    return { student: student, course: course }
 }
 
 const courseProgressHandler = (student, courseId, courseProgress) => {
-    for (let i=0; i < student.courses.length; i++) {
+    for (let i = 0; i < student.courses.length; i++) {
         const course = student.courses[i];
-        if(course._id === courseId){
+        if (course._id === courseId) {
             return course.progress;
-        } else if ((course._id !== courseId) && (i === student.courseslength - 1)){
+        } else if ((course._id !== courseId) && (i === student.courseslength - 1)) {
             return false;
         }
     }
 }
 
 const archiveCourse = async (studentId, courseId) => {
-    const student = await Student.findById({_id: studentId});
+    const student = await Student.findById({ _id: studentId });
     const newStudent = checkIfCourseExistsAndUpdate(student, courseId, true);
     return await newStudent.save();
 }
 
 const unArchiveCourse = async (studentId, courseId) => {
-    const student = await Student.findById({_id: studentId});
+    const student = await Student.findById({ _id: studentId });
     const newStudent = checkIfCourseExistsAndUpdate(student, courseId, false);
     return await newStudent.save();
 }
 
 const getCourseProgress = async (student, courseId) => {
-    const student = await Student.findById({_id: studentId});
+    const student = await Student.findById({ _id: studentId });
     return courseProgressHandler(student, courseId);
 }
 
 const setCourseProgress = async (student, courseId, courseProgress) => {
-    const student = await Student.findById({_id: studentId});
+    const student = await Student.findById({ _id: studentId });
     return courseProgressHandler(student, courseId, courseProgress);
+}
+
+const enrollInACourse = async (studentId, courseId) => {
+    const student = await Student.findById({ _id: studentId });
+    const course = await Student.findById({ _id: courseId });
+    const { student: newStudent, course: newCourse } = checkIfCourseExistsAndEnroll(student, course);
+    return (await newStudent.save()) && (await newCourse.save());
+}
+
+const unenrollFromACourse = async (studentId, courseId) => {
+    const student = await Student.findById({ _id: studentId });
+    const course = await Student.findById({ _id: courseId });
+    const { student: newStudent, course: newCourse } = checkIfCourseExistsAndUnEnroll(student, course);
+    return (await newStudent.save()) && (await newCourse.save());
 }
 
 module.exports = {
