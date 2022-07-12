3daaca1b6d7e7a3c1979bbff0e3c92bf7f97461b
Arshdeep Singh <arshtejay@gmail.com>
2022-07-12 07:55:33 -0300

    Testing Tutors CRUD

--- backend/src/api/tutors/models/Tutor.js
+++ backend/src/api/tutors/models/Tutor.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const Tutor = new mongoose.Schema({
     id: String,
-    name: String
+    name: String,
+    courses: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "Course",
+        },
+    ],
 });
 
 module.exports = mongoose.model('Tutor', Tutor);
