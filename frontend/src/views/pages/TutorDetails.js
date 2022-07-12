85e3d139e2a5447a8931d76554f02f5b03e78cdd
Arshdeep Singh <arshtejay@gmail.com>
2022-07-12 19:01:09 -0300

    Tutor description page

--- frontend/src/views/pages/TutorDetails.js
+++ frontend/src/views/pages/TutorDetails.js
@@ -13,7 +13,7 @@ import RefreshIcon from "@mui/icons-material/Refresh";
 import TSearchBar from "../../components/TSearchBar";
 import { useLocation, useParams } from "react-router";
 import CourseDetailsHeading from "../../components/CourseDetailsHeading";
-import CourseBanner from "../../components/CourseBanner";
+import TutorBanner from "../../components/TutorBanner";
 import { getAllCourses, getCourseDetails } from "./services/courses-rest";
 import { getAllTutors } from "./services/tutors-rest";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,16 +22,19 @@ export default function TutorDetails() {
     const isTutor = localStorage.getItem("isTutor") ? localStorage.getItem("isTutor") === "true" : true;
     const { id } = useParams();
     const dispatch = useDispatch();
-    const tutorDetails =  useSelector(state => state.tutor.allTutors).data;
-    console.log("Tutorrrss=>",tutorDetails);
-    const tutor = tutorDetails.filter( itr => itr._id === id);
-    console.log("Tutorrr=>", tutor)
-  
+    const tutorDetails = useSelector(state => state.tutor.allTutors).data;
+
+    const tutor = tutorDetails.find(itr => itr._id === id);
+
     useEffect(() => {
-      dispatch(getAllTutors());
+        dispatch(getAllTutors());
     }, [dispatch]);
-    
-    
-    return (<>Yooo</>
+
+
+    return (
+        <Paper sx={{ maxWidth: 936, margin: "auto", overflow: "hidden", padding: 2 }}>
+            <h1 className="mb-6">{tutor?.name || "John Doe"}</h1>
+            <TutorBanner tutorImage={tutor?.imageURL} name={tutor?.name} expertise={tutor?.expertise} rating={tutor?.rating} description={tutor?.description} courses={tutor?.courses}></TutorBanner>
+        </Paper>
     );
 }
