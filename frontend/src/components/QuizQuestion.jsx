ba55fc291cffd67e66a321b90ae48ed7e64bf6d4
Parth Shah <parthshahk@gmail.com>
2022-07-12 00:07:58 -0300

    quiz operations for tutor added

--- frontend/src/components/QuizQuestion.jsx
+++ frontend/src/components/QuizQuestion.jsx
@@ -7,24 +7,21 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
 import Typography from "@mui/material/Typography";
 
-const QuizQuestion = ({ id }) => {
+const QuizQuestion = ({ question }) => {
     return (
         <>
             <Grid item xs={12}>
-                <Typography variant="h6">Question {id}</Typography>
-                <Typography>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, eligendi voluptas deleniti recusandae, necessitatibus ad a reprehenderit doloribus numquam aut consequuntur laboriosam odit? Consequuntur possimus sed aut dicta laudantium rerum officia
-                    corrupti ipsa eos. Beatae quisquam consectetur adipisci excepturi cumque. Lorem ipsum dolor sit amet consectetur adipisicing elit. In sint error, tenetur repellendus dolorem unde molestiae iusto similique esse officia.
-                </Typography>
+                <Typography variant="h6">Question {question.id}</Typography>
+                <Typography>{question.question}</Typography>
             </Grid>
             <Grid item xs={12}>
                 <FormControl>
                     <FormLabel id="demo-radio-buttons-group-label">Your Answer</FormLabel>
                     <RadioGroup aria-labelledby="demo-radio-buttons-group-label" defaultValue="female" name="radio-buttons-group">
-                        <FormControlLabel value="opt1" control={<Radio />} label="Corporis, eligendi voluptas" />
-                        <FormControlLabel value="opt2" control={<Radio />} label="Beatae quisquam" />
-                        <FormControlLabel value="opt3" control={<Radio />} label="Lorem ipsum dolor sit amet" />
-                        <FormControlLabel value="opt4" control={<Radio />} label="Consequuntur possimus sed aut dicta laudantium" />
+                        <FormControlLabel checked={question.options[0].isCorrect} value={question.options[0].option} control={<Radio />} label={question.options[0].option} />
+                        <FormControlLabel checked={question.options[1].isCorrect} value={question.options[1].option} control={<Radio />} label={question.options[1].option} />
+                        <FormControlLabel checked={question.options[2].isCorrect} value={question.options[2].option} control={<Radio />} label={question.options[2].option} />
+                        <FormControlLabel checked={question.options[3].isCorrect} value={question.options[3].option} control={<Radio />} label={question.options[3].option} />
                     </RadioGroup>
                 </FormControl>
             </Grid>
